5999a46f582fa00e23986c7ffb6a8d63152a74c4
Vikram <55603725+vikragha@users.noreply.github.com>
2023-12-19 02:22:36 -0500

    Update ansatz.py

--- utils/ansatz.py
+++ utils/ansatz.py
@@ -1,1 +1,32 @@
+# utils/ansatz.py
+class AnsatzRepresentations:
+    @staticmethod
+    def hardware_efficient_ansatz(qubits):
+        """
+        Generate the mathematical representation for Hardware Efficient Ansatz (HEA).
+
+        Parameters:
+        - qubits (int): Number of qubits.
+
+        Returns:
+        - str: Mathematical representation.
+        """
+        # Implement HEA representation
+        pass
+
+    @staticmethod
+    def alternating_layered_ansatz(qubits):
+        """
+        Generate the mathematical representation for Alternating Layered Ansatz (ALT).
+
+        Parameters:
+        - qubits (int): Number of qubits.
+
+        Returns:
+        - str: Mathematical representation.
+        """
+        # Implement ALT representation
+        pass
+
+    # Repeat for other ansätze...
 
